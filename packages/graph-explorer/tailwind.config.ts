847214a2003df298bd6e7a1d91d9f0ce05afb7b6
Kris McGinnes <kmcginnes@me.com>
2024-04-27 14:13:39 -0500

    Install tailwind

---
+++ packages/graph-explorer/tailwind.config.ts
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+import { type Config } from "tailwindcss";
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
