95f4bb6d06e0e37f3b7f16e150c6f85339e18548
Kris McGinnes <kmcginnes@me.com>
2024-06-08 17:49:42 -0500

    Add Jotai provider

--- packages/graph-explorer/src/core/StateProvider/StateProvider.tsx
+++ packages/graph-explorer/src/core/StateProvider/StateProvider.tsx
@@ -2,15 +2,18 @@ import { PropsWithChildren } from "react";
 import { RecoilRoot } from "recoil";
 import StateDebug from "./StateDebug";
 import { env } from "../../utils";
+import { Provider } from "jotai";
 
 const StateProvider = ({
   children,
 }: PropsWithChildren<Record<string, unknown>>) => {
   return (
-    <RecoilRoot>
-      {children}
-      {env.DEV && <StateDebug />}
-    </RecoilRoot>
+    <Provider>
+      <RecoilRoot>
+        {children}
+        {env.DEV && <StateDebug />}
+      </RecoilRoot>
+    </Provider>
   );
 };
 
