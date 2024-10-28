10d0f41f6fd57183e17b1d9493d358eaf4dcce04
Kris McGinnes <kmcginnes@me.com>
2024-10-28 11:14:42 -0500

    Update graph empty state

--- packages/graph-explorer/src/components/Graph/internalComponents/EmptyState.tsx
+++ packages/graph-explorer/src/components/Graph/internalComponents/EmptyState.tsx
@@ -1,13 +1,20 @@
 import { SearchIcon } from "@/components/icons";
-import { PanelEmptyState } from "@/components/PanelEmptyState";
+import { PlusCircleIcon } from "lucide-react";
 
 const EmptyState = () => {
   return (
-    <div className="pointer-events-none absolute inset-0 flex select-none flex-col items-center justify-center bg-gray-100/60 p-4">
-      <PanelEmptyState
-        icon={<SearchIcon />}
-        title="To get started, click into the search bar to browse graph data. Click + to add to Graph View."
-      />
+    <div className="flex h-full w-full flex-col items-center justify-center gap-8">
+      <div className="bg-primary-dark/75 size-24 rounded-full p-5">
+        <SearchIcon className="h-full w-full text-white/75" />
+      </div>
+      <div className="text-primary-dark max-w-lg space-y-2 text-center text-lg">
+        <div className="text-xl font-bold">Search For Vertices</div>
+        <div className="text-balance leading-snug">
+          To get started, use the search sidebar panel to browse graph data.
+          Click the <PlusCircleIcon className="-mt-1 inline size-5" /> to add to
+          Graph View.
+        </div>
+      </div>
     </div>
   );
 };
